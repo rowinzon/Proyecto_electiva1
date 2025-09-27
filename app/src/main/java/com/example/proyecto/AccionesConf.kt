6ea79951686c01d31655f688cc202f6d3ffae4af
ROWINZON <RTELLEZ20@UAN.EDU.CO>
2025-09-26 20:44:12 -0500

    para que mi conpañero suba lo que hizo

--- app/src/main/java/com/example/proyecto/AccionesConf.kt
+++ app/src/main/java/com/example/proyecto/AccionesConf.kt
@@ -1,2 +1,71 @@
 package com.example.proyecto
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun AccionesConf() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        var isSelected by remember { mutableStateOf(false) }
+
+        // Título
+        Column(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 100.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Settings", fontSize = 30.sp, color = Color.Black)
+        }
+
+        // Card con texto y botón
+        Card(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxWidth(0.9f)
+                .wrapContentHeight(),
+            colors = CardDefaults.cardColors(containerColor = Color.LightGray),
+            elevation = CardDefaults.cardElevation(8.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Funciona", fontSize = 20.sp, color = Color.Blue)
+
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("Push Notification", color = Color.Black)
+                    Spacer(modifier = Modifier.weight(1f))
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Button(
+                            onClick = { isSelected = !isSelected },
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = if (isSelected) Color.Blue else Color.Gray
+                            )
+                        ) {
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PantallaAccionesPreview() {
+    AccionesConf()
+}
